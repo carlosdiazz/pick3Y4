547e5a702da8567936146989f885b67bef268cd1
carlosdiazz <c.diazadriann@gmail.com>
2022-07-27 16:12:07 -0400

    Cambios Variables

--- TELEGRAM/TELEGRAM.py
+++ TELEGRAM/TELEGRAM.py
@@ -1,15 +1,17 @@
-TELEGRAM_TOKEN_PADRE = '5348496240:AAHvkD64i5AveuGv3v_5K1y5AyPn74MOqVg'
+from ..config import BOT_TELEGRAM_PADRE
+TOKEN = BOT_TELEGRAM_PADRE['TOKEN']
 
 
-import telebot
+import telebot, time
+import threading
 
 #Instanciamos el Bot
-bot = telebot.TeleBot(TELEGRAM_TOKEN_PADRE)
+bot = telebot.TeleBot(TOKEN)
 
 #Responde el comando Start
 @bot.message_handler(commands=['start', 'ayuda', 'help'])
 def cmd_start(message):
-    print("Entro en la fun cion Start")
+    print("Entro en la funcion Start")
     bot.reply_to(message, 'HOLA QLOQ')
 
 
@@ -27,11 +29,21 @@ def bot_mensajes_texto(message):
     if message.text.startswith('/'):
         bot.send_message(message.chat.id, 'Comando No Disponible')
     else:
+        bot.send_chat_action(message.chat.id, 'typing') # AQUI AGREGO QUE ESTE ESCRIBIENDO
+        time.sleep(1)
         bot.send_message(message.chat.id, message_a_enviar, parse_mode='html')
 
+def recibir_mensajes():
+    bot.infinity_polling()
 
 # MAIN ------------------
 if __name__ == '__main__':
+    bot.set_my_commands([
+        telebot.types.BotCommand('/start', 'Da la Bienvenida'),
+        telebot.types.BotCommand('/start2', 'Da la Bienvenida'),
+        telebot.types.BotCommand('/start3', 'Da la Bienvenida')
+    ])
     print("INICIO EL BOT PADRE")
-    bot.infinity_polling()
+    hilo_bot = threading.Thread(name='hilo_bot', target=recibir_mensajes)
+    hilo_bot.start()
     print("QLOQ")
