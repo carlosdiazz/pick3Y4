855a81ebdca2fdb1ce7685a131e9d592b67c9d6f
carlosdiazz <c.diazadriann@gmail.com>
2022-12-02 13:17:01 -0400

    Fix Xpath PRIMERA, REAL, LOTEKA

--- DATOS_Loterias_Dominicana/LOTEKA.py
+++ DATOS_Loterias_Dominicana/LOTEKA.py
@@ -1,10 +1,10 @@
 from DATOS_Loterias_Dominicana.LOTERIAS_DOMINICAS_PAGES import *
 
 PAGES       = 'https://loteka.com.do/'
-FECHA       = '/html/body/div[7]/div[1]/div/div/div[1]/div[2]/div[5]/div[2]'
-NU_1        = '/html/body/div[7]/div[1]/div/div/div[1]/div[2]/div[5]/div[3]/div[1]'
-NU_2        = '/html/body/div[7]/div[1]/div/div/div[1]/div[2]/div[5]/div[3]/div[2]'
-NU_3        = '/html/body/div[7]/div[1]/div/div/div[1]/div[2]/div[5]/div[3]/div[3]'
+FECHA       = '/html/body/div[7]/div[1]/div/div/div[1]/div[2]/div[4]/div[2]'
+NU_1        = '/html/body/div[7]/div[1]/div/div/div[1]/div[2]/div[4]/div[3]/div[1]'
+NU_2        = '/html/body/div[7]/div[1]/div/div/div[1]/div[2]/div[4]/div[3]/div[2]'
+NU_3        = '/html/body/div[7]/div[1]/div/div/div[1]/div[2]/div[4]/div[3]/div[3]'
 
 LOTEKA = {
     'URL' : [PAGES, PAGES],
