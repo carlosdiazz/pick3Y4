8dceb3c23fbbd2197872a5cc6d27075cc55383fb
carlos <c.diazadriann@gmail.com>
2023-01-30 10:14:56 -0400

    fd

--- API_USA_PICK.py
+++ API_USA_PICK.py
@@ -16,7 +16,7 @@ class API_USA_PICK():
     def Navegador(self,ARR):
         self.chrome_options = webdriver.ChromeOptions()
         self.chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
-        #self.chrome_options.add_argument("--headless")
+        self.chrome_options.add_argument("--headless")
         self.driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()),options=self.chrome_options)
         self.driver.delete_all_cookies()
         self.driver.get(ARR[f'URL_{self.sorteo}'][0])
